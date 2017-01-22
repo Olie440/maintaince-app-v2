e76139fcee51f3e7d3ece519bbfd4b9903586b66
Oliver Rushworth <olie440@msn.com>
2017-01-22 22:23:52 +0000

    [Issue #5] - Add mockup file upload component

---
+++ src/components/file-upload/component.js
@@ -0,0 +1,26 @@
+import template from './template.html';
+
+
+const bindings = {
+    model: '='
+};
+
+
+function controller($scope, $element) { 
+    const button = $element[0].querySelector('button');
+    const input = $element[0].querySelector('input');
+
+    $scope.compressionSupport = true;
+    $scope.actions = {
+        showFileDialog(event) {
+            event.preventDefault();
+            input.click();
+        }
+    };
+}
+
+export default {
+    template,
+    bindings,
+    controller: [ '$scope', '$element', controller ]
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,11 @@ import angular from 'angular';
 
 import officeStatus from './office-status/component.js';
 import dropdownWithTextbox from './dropdown-with-textbox/component.js';
+import fileUpload from './file-upload/component.js';
 
 const module = angular
 	.module('MaintenanceApp.components', [])
     .component('officeStatus', officeStatus)
-    .component('dropdownWithTextbox', dropdownWithTextbox);
+    .component('fileUpload', fileUpload);
 
 export default module
