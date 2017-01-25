8db2b1295ca503e74e87fe5a54dc20ea08e2050b
Oliver Rushworth <olie440@msn.com>
2017-01-25 10:31:35 +0000

    [Issue #6] - Optimise karma config

--- karma.conf.js
+++ karma.conf.js
@@ -33,12 +33,14 @@ module.exports = function(config) {
         'test/**/*.js': ['webpack']
     },
 
-    webpack: webpackConfig,
+    webpack: {
+        module: webpackConfig.module
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots'],
 
 
     // web server port
